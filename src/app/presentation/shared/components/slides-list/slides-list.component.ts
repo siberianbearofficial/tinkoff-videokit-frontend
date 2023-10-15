e7491b3fc96ec237b15bd01de1075a218b78c093
Aleksei Orlov <alexeyorlov65@gmail.com>
2023-10-15 20:40:59 +0300

    Slides List Component

---
+++ src/app/presentation/shared/components/slides-list/slides-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slides-list',
+  templateUrl: './slides-list.component.html',
+  styleUrls: ['./slides-list.component.scss']
+})
+export class SlidesListComponent {
+
+}
