43edc57d016e1b6d0fa761f3c6cfeb698438670a
Aleksei Orlov <alexeyorlov65@gmail.com>
2023-10-15 20:40:40 +0300

    Images List Component

---
+++ src/app/presentation/shared/components/images-list/images-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-images-list',
+  templateUrl: './images-list.component.html',
+  styleUrls: ['./images-list.component.scss']
+})
+export class ImagesListComponent {
+
+}
