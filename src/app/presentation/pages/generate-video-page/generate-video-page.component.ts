7f6491cf47ead9c36cfa87a2e8053e861d52968b
Aleksei Orlov <alexeyorlov65@gmail.com>
2023-10-16 23:25:56 +0300

    Generate Video Page

--- src/app/presentation/pages/generate-video-page/generate-video-page.component.ts
+++ src/app/presentation/pages/generate-video-page/generate-video-page.component.ts
@@ -1,10 +1,14 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {GenerateVideoService} from "../../../core/usecases/interactors/generate-video.service";
+import {ProjectsService} from "../../../core/usecases/interactors/projects.service";
 import {catchError, Observable, of, Subscription, switchMap} from "rxjs";
-import {RedirectService} from "../../../infrastructure/adapters/services/redirect.service";
 // import {signInPageUrl} from "../../../app-routing.module";
 // import {AuthenticationService} from "../../../core/usecases/interactors/authentication.service";
-import {Video} from "../../../core/domain/entities/video";
+import {Project} from "../../../core/domain/entities/project";
+import {RedirectService} from "../../../infrastructure/adapters/services/redirect.service";
+import {Dialog} from "@angular/cdk/dialog";
+import {VideoModalComponent} from "../../shared/components/video-modal/video-modal.component";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {ShareVideoModalComponent} from "../../shared/components/share-video-modal/share-video-modal.component";
 
 
 @Component({
@@ -15,53 +19,27 @@ import {Video} from "../../../core/domain/entities/video";
 export class GenerateVideoPageComponent implements OnInit, OnDestroy {
 
   // private isSignedInSubscription!: Subscription;
-  private generateVideoSubscription!: Subscription;
+  private getProjectSubscription!: Subscription;
+  private updateProjectSubscription!: Subscription;
+  private videoModalOnCloseSubscription!: Subscription;
+  private shareVideoModalOnCloseSubscription!: Subscription;
 
-  // public step: number = 0;
   public error: string = '';
 
-  // public videoUrl: string = '';
+  public project?: Project;
 
-  // public rateStarSelected: number = 0;
-
-  // public avatars = [
-  //   {
-  //     image_url: '/static/images/avatars/avatar1.png'
-  //   },
-  //   {
-  //     image_url: '/static/images/avatars/avatar2.png'
-  //   },
-  //   {
-  //     image_url: '/static/images/avatars/avatar3.png'
-  //   },
-  //   {
-  //     image_url: '/static/images/avatars/avatar4.png'
-  //   }
-  // ];
-  // public avatarIndex: number = 0;
-
-  public images = [
-    {
-      image_url: 'src/assets/images/backgrounds/background1.jpg'
-    },
-    {
-      image_url: 'src/assets/images/backgrounds/background2.jpg'
-    },
-    {
-      image_url: 'src/assets/images/backgrounds/background3.jpg'
-    }
-  ];
-  public imageIndex: number = 0;
-
-  public avatarPositions: string[] = [
+  public avatarTypes: string[] = [
     'слева снизу',
     'в центре',
     'справа'
   ];
-  public avatarPositionIndex: number = 1;
 
-  constructor(private generateVideoService: GenerateVideoService,
+  public currentSlideIndex: number = 0;
+
+  constructor(private projectsService: ProjectsService,
               private redirectService: RedirectService,
+              private modalService: Dialog,
+              private route: ActivatedRoute
               /*private authenticationService: AuthenticationService*/) {
   }
 
@@ -76,46 +54,101 @@ export class GenerateVideoPageComponent implements OnInit, OnDestroy {
     //     })
     //   )
     //   .subscribe();
+    this.getProjectSubscription = this.route.paramMap
+      .pipe(
+        switchMap((params: ParamMap) => {
+          const id: string | null = params.get('id');
+          if (id)
+            return this.projectsService.getProject(id);
+          throw new Error('Необходимо передать ID проекта');
+        }),
+        catchError((error: Error) => {
+          this.showError(error);
+          return of(false);
+        })
+      )
+      .subscribe((project: Project | boolean): void => {
+        if (typeof project != 'boolean') {
+          this.project = project;
+          this.hideError();
+        }
+      })
+  }
+
+  public onGenerateButtonClick() {
+    if (this.project) {
+      this.updateProjectSubscription = this.projectsService.updateProject(this.project)
+        .pipe(
+          catchError((error: Error) => {
+            this.showError(error);
+            return of(false);
+          }),
+          // switchMap((result: boolean | void) => {
+          //   return this.redirectService.redirectIf(typeof result != 'boolean', '/projects');
+          // }),
+          // catchError((error: Error) => {
+          //   this.showError(error);
+          //   return of(false);
+          // })
+        )
+        .subscribe((result: boolean | void): void => {
+          if (typeof result != 'boolean')
+            this.hideError();
+        });
+    }
+  }
+
+  public onPlayButtonClick(): void {
+    if (this.project) {
+      this.videoModalOnCloseSubscription = this.modalService.open(VideoModalComponent, {
+        panelClass: 'modal-container',
+        width: "70%",
+        data: {
+          'video_url': this.project.processedVideo
+        }
+      }).closed.subscribe();
+    }
+  }
+
+  public onQrButtonClick(): void {
+    if (this.project) {
+      this.shareVideoModalOnCloseSubscription = this.modalService.open(ShareVideoModalComponent, {
+        panelClass: 'modal-container',
+        width: "40%",
+        data: {
+          'video_url': this.project.processedVideo
+        }
+      }).closed.subscribe();
+    }
+  }
+
+  public onChangeAvatarPositionButtonClick(): void {
+    if (this.project && this.project.slides) {
+      if (this.project.slides[this.currentSlideIndex].avatarType < this.avatarTypes.length - 1)
+        this.project.slides[this.currentSlideIndex].avatarType++;
+      else
+        this.project.slides[this.currentSlideIndex].avatarType = 0;
+    }
   }
 
   public showError(error: Error): void {
     this.error = error.message;
   }
 
-  public onChangeAvatarPositionButtonClick(): void {
-    this.avatarPositionIndex = (this.avatarPositionIndex == this.avatarPositions.length - 1) ? 0 : (this.avatarPositionIndex + 1);
-  }
-
-  // public onGenerateButtonClick(description: string) {
-  //   this.generateVideoSubscription = this.generateVideoService.generateVideo(this.avatarIndex, this.alignmentIndex, description, this.backgroundIndex, this.musicBlob, this.avatarPositionIndex)
-  //     .pipe(
-  //       catchError((error) => {
-  //         this.showError(error);
-  //         return of(false);
-  //       }),
-  //       switchMap((status) => {
-  //         if (status != false) {
-  //           this.step = 1;
-  //           this.videoUrl = (status as Video).videoUrl;
-  //         }
-  //         return of(status);
-  //       })
-  //     )
-  //     .subscribe();
-  // }
-
-  // public setRateStarSelected(rateStarSelected: number) {
-  //   this.rateStarSelected = rateStarSelected;
-  // }
-
-  public setImageIndex(imageIndex: number): void {
-    this.imageIndex = imageIndex;
+  public hideError(): void {
+    this.error = '';
   }
 
   public ngOnDestroy() {
     // if (this.isSignedInSubscription)
     //   this.isSignedInSubscription.unsubscribe();
-    if (this.generateVideoSubscription)
-      this.generateVideoSubscription.unsubscribe();
+    if (this.getProjectSubscription)
+      this.getProjectSubscription.unsubscribe();
+    if (this.updateProjectSubscription)
+      this.updateProjectSubscription.unsubscribe();
+    if (this.videoModalOnCloseSubscription)
+      this.videoModalOnCloseSubscription.unsubscribe();
+    if (this.shareVideoModalOnCloseSubscription)
+      this.shareVideoModalOnCloseSubscription.unsubscribe();
   }
 }
