b82dd24315518cfb16fff9a47af78fd61f757f67
Aleksei Orlov <alexeyorlov65@gmail.com>
2023-10-23 19:42:43 +0300

    Slides list

--- src/app/presentation/shared/components/slides-list/slides-list.component.ts
+++ src/app/presentation/shared/components/slides-list/slides-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Slide} from "../../../../core/domain/entities/slide";
 
 @Component({
   selector: 'app-slides-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./slides-list.component.scss']
 })
 export class SlidesListComponent {
+  @Input() slides: Slide[] = [];
+  @Input() selectedIndex: number = 0;
+  @Output() selectedIndexChange = new EventEmitter<number>();
 
+  constructor() { }
+
+  selectSlide(index: number): void {
+    this.selectedIndex = index;
+    this.selectedIndexChange.emit(index);
+  }
 }
