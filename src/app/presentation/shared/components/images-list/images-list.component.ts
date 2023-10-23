efc0359a3d8d21ce862cd02b2b04f2c0ba04024f
Aleksei Orlov <alexeyorlov65@gmail.com>
2023-10-23 18:40:32 +0300

    Images list

--- src/app/presentation/shared/components/images-list/images-list.component.ts
+++ src/app/presentation/shared/components/images-list/images-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-images-list',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./images-list.component.scss']
 })
 export class ImagesListComponent {
+  @Input() mediaUrls: string[] = []; // тут список ссылок на медиафайлы
+  @Input() selectedIndex: number = 0; // индекс выбранного фона
 
+  @Output() selectedImageIndexChange: EventEmitter<number> = new EventEmitter<number>();
+  constructor() { }
+
+  selectImage(index: number): void {
+    this.selectedIndex = index; // обновление выбранного фона
+    this.selectedImageIndexChange.emit(index); // создаем событие выбранного фона
+  }
 }
